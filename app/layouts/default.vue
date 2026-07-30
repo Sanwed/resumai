226424761692e1c0ccfea87d417a1a35e05d7b1e
Александр Козюков <a.kozyukov@qsoft.ru>
2026-07-30 11:22:51 +0400

    feat: added cookie banner

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,3 +1,34 @@
+<script lang="ts" setup>
+  const toast = useToast();
+
+  onMounted(async () => {
+    const cookie = useCookie('cookie-consent');
+    if (cookie.value === 'accepted') {
+      return;
+    }
+
+    toast.add({
+      title: 'We use first-party cookies to enhance your experience on our website.',
+      duration: 0,
+      color: 'info',
+      icon: 'i-lucide-cookie',
+      close: false,
+      actions: [
+        {
+          label: 'Accept',
+          color: 'primary',
+          variant: 'solid',
+          size: 'lg',
+          class: 'min-w-30 justify-center',
+          onClick: () => {
+            cookie.value = 'accepted';
+          },
+        },
+      ],
+    });
+  });
+</script>
+
 <template>
   <div>
     <AppHeader />
